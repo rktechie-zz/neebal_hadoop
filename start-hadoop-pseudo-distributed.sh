01dd5916a205c03d469dfd2894d51f8e3a2337a1
Rishabh Kedia <rishabhkedia.92@gmail.com>
2015-08-08 22:59:18 +0530

    HBase files

---
+++ start-hadoop-pseudo-distributed.sh
@@ -0,0 +1,5 @@
+yarn-daemon.sh start resourcemanager
+yarn-daemon.sh start nodemanager
+hadoop-daemon.sh start namenode
+hadoop-daemon.sh start datanode
+mr-jobhistory-daemon.sh start historyserver
---
+++ stop-hadoop-pseudo-distributed.sh
@@ -0,0 +1,5 @@
+yarn-daemon.sh stop resourcemanager
+yarn-daemon.sh stop nodemanager
+hadoop-daemon.sh stop namenode
+hadoop-daemon.sh stop datanode
+mr-jobhistory-daemon.sh stop historyserver
